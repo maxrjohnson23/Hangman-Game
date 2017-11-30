ad1f24395230d2f2ebee1eccf63abfcfe701e9d5
Max <lfmax223@gmail.com>
2017-11-30 16:29:58 -0600

    Update car theme and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 document.addEventListener('keydown', function(event) {
     // check if key is a letter a-z or space to start game
-    console.log(event.key);
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
+    if ((event.keyCode >= 65 && event.keyCode <= 90) && !game.isGameOver()) {
         game.guessLetter(String.fromCharCode(event.keyCode));
     } else if (event.key === " ") {
         game.startGame();
@@ -11,37 +10,19 @@ document.addEventListener('keydown', function(event) {
 });
 
 
-function fadeIn(element) {
-    // initial opacity
-    var op =0.1;
-    element.style.visibility = 'visible';
-    element.style.opacity = op;
-    // increase opacity over time
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
-}
-
-
-var game = new Game();
-
 function Game() {
     var lettersGuessed = [];
     var guessWord = [];
     var maskedWord = [];
     var remainingGuesses = 6;
     var startingGuesses = 6;
-    var screenHandler = new ScreenHandler();
     var wins = 0;
     var losses = 0;
+    var gameOver = false;
 
     this.startGame = function() {
-        console.log("Starting Game");
+        // unlock game after resetting delay
+        gameOver = false;
         screenHandler.hideInstructions();
         // Reset data for new game
         guessWord = [];
@@ -49,6 +30,8 @@ function Game() {
         lettersGuessed = [];
         remainingGuesses = 6;
         this.createWord();
+        // Reset screen for new game
+        screenHandler.drawPlatform();
         screenHandler.updateGuessWord(maskedWord);
         screenHandler.updateRemainingGuesses(startingGuesses);
         screenHandler.updateGuessedLetters(lettersGuessed);
@@ -59,9 +42,14 @@ function Game() {
     this.createWord = function() {
         var word = new WordGenerator().generateWord();
         guessWord = word.toUpperCase().split("");
-        console.log("Word is: " + guessWord);
+        console.log("SPOILER!: The Word is: " + guessWord);
         for (var i = 0; i < guessWord.length; i++) {
-            maskedWord.push("_");
+            // Account for multiple-word answers
+            if (guessWord[i] === " ") {
+                maskedWord.push(" ");
+            } else {
+                maskedWord.push("_");
+            }
         }
     };
 
@@ -74,15 +62,16 @@ function Game() {
             if (guessWord.includes(letter)) {
                 this.updateWord(letter);
                 isCorrect = true;
-            } 
+            }
             this.updateGuesses(isCorrect);
             this.checkGameOver();
         }
-       
+
     };
 
     this.updateWord = function(guessedLetter) {
 
+        // Replace underscores as the word is uncovered
         for (var i = 0; i < guessWord.length; i++) {
             if (guessWord[i] === guessedLetter) {
                 maskedWord[i] = guessedLetter;
@@ -95,6 +84,8 @@ function Game() {
     this.updateGuesses = function(isCorrect) {
         if (!isCorrect) {
             remainingGuesses--;
+            // Update hangman drawing
+            screenHandler.drawHangman(remainingGuesses);
         }
         screenHandler.updateGuessedLetters(lettersGuessed);
         screenHandler.updateRemainingGuesses(remainingGuesses);
@@ -103,64 +94,160 @@ function Game() {
 
     this.checkGameOver = function() {
         if (!maskedWord.includes("_")) {
-            console.log("WIN!");
             wins++;
-            //screenHandler.updateWins(wins);
+            // freeze game during animation
+            gameOver = true;
+            screenHandler.flashBanner("win");
             screenHandler.winAnimation();
-            this.startGame();
+            // scope trick to pass in this 
+            var that = this;
+            setTimeout(function() {
+                that.startGame();
+            }, 3000);
+
         } else if (remainingGuesses === 0) {
-            console.log("LOSE!");
             losses++;
-            //screenHandler.updateLosses(losses);
-            this.startGame();
+            gameOver = true;
+            screenHandler.flashBanner("lose");
+            var that = this;
+            setTimeout(function() {
+                that.startGame();
+            }, 3000);
         }
-    }
+    };
+
+    this.isGameOver = function() {
+        return gameOver;
+    };
 
 }
 
-function ScreenHandler() {
-    this.hideInstructions = function() {
-        document.getElementById("instructions").style.visibility = "hidden";
-    }
-    this.updateGuessWord = function(maskedWord) {
-        // array to string for display
+var screenHandler = {
+    hideInstructions : function() {
+        var instructions = document.getElementById("instructions");
+        instructions.style.visibility = "hidden";
+        instructions.classList.remove("animated");
+
+    },
+    updateGuessWord : function(maskedWord) {
+        // aremove commas when displaying on screen
         document.getElementById("guess-word").innerHTML = maskedWord.join("");
-    };
-
-    this.updateGuessedLetters = function(letters) {
+    },
+    updateGuessedLetters : function(letters) {
         document.getElementById("letters-guessed").innerHTML = letters;
-    };
+    },
 
-    this.updateRemainingGuesses = function(numGuesses) {
+    updateRemainingGuesses : function(numGuesses) {
         document.getElementById("guesses-remaining").innerHTML = numGuesses;
-    };
+    },
 
-    this.updateWins = function(wins) {
+    updateWins : function(wins) {
         document.getElementById("wins").innerHTML = wins;
-    }
+    },
 
-    this.updateLosses = function(losses) {
+    updateLosses : function(losses) {
         document.getElementById("losses").innerHTML = losses;
-    }
-    this.displayWinBanner = function() {
-        var banner = document.getElementById("alert-banner");
-        banner.innerHTML = "<p><strong>You win!<strong></p>";
-        // fade(banner);
-        // banner.style.visibility = 'hidden';
-        fadeIn(banner);
-    }
-    this.winAnimation = function() {
+    },
+
+    winAnimation : function() {
         var audio = new Audio('assets/sounds/vroom.mp3');
         audio.play();
         var car = document.getElementById("car");
-        var win = document.getElementById("win");
-        // trick to reset for multiple animations
+        // trick to reset animations for next run
         car.classList.remove("zoomAround");
-        win.classList.remove("flash");
         car.offsetHeight;
-        win.offsetHeight;
-        win.classList.add("flash");
         car.classList.add("zoomAround");
+    },
 
+    flashBanner : function(outcome) {
+        var audio = new Audio('assets/sounds/tires.mp3');
+        audio.play();
+        var banner = document.getElementById("banner");
+        if (outcome === "lose") {
+            banner.innerHTML = "You Lose!!";
+        } else {
+            banner.innerHTML = "You Win!!";
+        }
+        banner.classList.remove("flash");
+        // reset CSS animation
+        banner.offsetHeight;
+        banner.classList.add("flash");
+
+    },
+
+    drawPlatform : function() {
+        var c = document.getElementById("canvas");
+        var ctx = c.getContext("2d");
+        // clear canvas for new game
+        ctx.beginPath();
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.lineWidth = 4;
+        // base
+        ctx.moveTo(75, 200);
+        ctx.lineTo(225, 200);
+        // vertical 
+        ctx.moveTo(100, 200);
+        ctx.lineTo(100, 50);
+        // top
+        ctx.moveTo(100, 50);
+        ctx.lineTo(175, 50);
+        // rope
+        ctx.moveTo(175, 50);
+        ctx.lineTo(175, 75);
+        ctx.stroke();
+    },
+
+    drawHangman : function(guessNumber) {
+        var c = document.getElementById("canvas");
+        var ctx = c.getContext("2d");
+        ctx.lineWidth = 4;
+
+        console.log("Guess: " + guessNumber);
+
+        switch (guessNumber) {
+            case 5:
+                // draw head
+                ctx.beginPath();
+                ctx.arc(175, 95, 20, 0, 2 * Math.PI);
+                ctx.stroke();
+                break;
+            case 4: 
+                // draw body
+                ctx.moveTo(175, 115);
+                ctx.lineTo(175, 160);
+                ctx.stroke();
+                break;
+            case 3: 
+                // draw left arm
+                ctx.moveTo(175, 130);
+                ctx.lineTo(145, 120);
+                ctx.stroke();
+                break;
+            case 2: 
+                // draw right arm
+                ctx.moveTo(175, 130);
+                ctx.lineTo(205, 120);
+                ctx.stroke();
+                break;
+            case 1: 
+                // draw left leg
+                ctx.moveTo(175, 160);
+                ctx.lineTo(145, 190);
+                ctx.stroke();
+                break;
+            case 0: 
+                // draw right leg
+                ctx.moveTo(175, 160);
+                ctx.lineTo(205, 190);
+                ctx.stroke();
+                break;
+
+        }       
     }
 }
+
+// Initialize the canvas drawing
+var game = new Game();
+screenHandler.drawPlatform();
